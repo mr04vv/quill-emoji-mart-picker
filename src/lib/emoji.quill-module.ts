e2bc09ff9b394c02915cf1de9e9ee0036924687a
Takuto Mori <mr04vv@gmail.com>
2021-08-15 22:55:27 +0900

    chore: ieで動くように

--- src/lib/emoji.quill-module.ts
+++ src/lib/emoji.quill-module.ts
@@ -56,7 +56,10 @@ export class EmojiModule extends Module {
     if (this.options.convertEmoticons) {
       // Emoticons to Emoji
       replacements.push({
-        regex: new RegExp(Emoji.emoticonRe, "g"),
+        regex:
+          typeof Emoji.emoticonRe === "string"
+            ? new RegExp(Emoji.emoticonRe, "g")
+            : new RegExp(Emoji.emoticonRe),
         matchIndex: 1,
         replacementIndex: 1,
         fn: (str: string) => Emoji.emoticonToEmoji(str),
@@ -66,7 +69,10 @@ export class EmojiModule extends Module {
     if (this.options.convertShortNames) {
       // ShortNames to Emoji
       replacements.push({
-        regex: new RegExp(Emoji.shortNameRe, "g"),
+        regex:
+          typeof Emoji.shortNameRe === "string"
+            ? new RegExp(Emoji.shortNameRe, "g")
+            : new RegExp(Emoji.shortNameRe),
         matchIndex: 2,
         replacementIndex: 1,
         fn: (str: string) => Emoji.shortNameToEmoji(str),
